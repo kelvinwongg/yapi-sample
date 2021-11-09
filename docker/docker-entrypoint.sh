55a09957b52ef8a9f0721520b67c6569dc2210b3
Kelvin@MBP <kelvin.wyh@tti.com.hk>
2021-11-09 17:40:06 +0800

    init commit

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cp "/var/www/html/sample-project/docker/docker-php-custom.ini" "$PHP_INI_DIR/conf.d/"
+apt-get update && apt-get install -y libyaml-dev \
+&& pecl install xdebug && pecl install yaml \
+&& docker-php-ext-enable xdebug yaml
+/usr/local/bin/docker-php-entrypoint apache2-foreground
