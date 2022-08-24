8091cb75b5346d6e613ecb9f44e7146f3d6ec747
kelvinwong <KelvinYH_Wong@cncbinternational.com>
2022-08-24 17:30:26 +0800

    Yapi::checkCrud beta

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
 
+namespace YapiSample;
+
 require_once __DIR__ . '/vendor/autoload.php';
 
-// $yapi = new Kelvinwongg\Yapi\Yapi();
-// $yapi = new Kelvinwongg\Yapi\Yapi('invalid/file/path');
-// $yapi = new Kelvinwongg\Yapi\Yapi('./endpoints/HR_API_OAS.yaml');
-$yapi = new Kelvinwongg\Yapi\Yapi('endpoints/HR_API_OAS.yaml');
+$yapi = new \Kelvinwongg\Yapi\Yapi('oas/HR_API_OAS.yaml');
---
+++ paths/employees.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace YapiSample;
+
+class Employees
+{
+	public function __construct()
+	{
+		echo __NAMESPACE__ . '\Employees::__construct()' . '<br>';
+	}
+}
