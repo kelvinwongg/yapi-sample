7ecd093274a972764f16888345b1de5e920ee2c1
kelvinwong <KelvinYH_Wong@cncbinternational.com>
2023-01-11 11:43:55 +0800

    Employees::get response JSON

--- paths/employees.php
+++ paths/employees.php
@@ -11,6 +11,17 @@ class Employees
 	
 	public function get($file, $request, $response, $crudHook)
 	{
-		$response->setContent('added from Employees::get');
+		$response->setContent([
+			[
+				'id' => 1,
+				'employee_name' => 'Steve Davis',
+				'employee_title' => 'Staff'
+			],
+			[
+				'id' => 2,
+				'employee_name' => 'Ronnie Hart',
+				'employee_title' => 'Admin'
+			]
+		]);
 	}
 }
