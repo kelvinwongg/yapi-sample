9448327936eacc7bc10be08d1d06e0489b1eeb99
kelvinwong <KelvinYH_Wong@cncbinternational.com>
2023-01-11 11:54:17 +0800

    housekeeping

--- index.php
+++ index.php
@@ -3,5 +3,4 @@
 namespace YapiSample;
 
 require_once __DIR__ . '/vendor/autoload.php';
-
-$yapi = new \Kelvinwongg\Yapi\Yapi('oas/HR_API_OAS.yaml');
+$yapi = new \Kelvinwongg\Yapi\Yapi('oas/api.yaml');
--- paths/employees.php
+++ paths/employees.php
@@ -3,12 +3,7 @@
 namespace YapiSample;
 
 class Employees
-{
-	public function __construct($file, $request, $response, $crudHook)
-	{
-		$response->setContent('added from Employees::__construct');
-	}
-	
+{	
 	public function get($file, $request, $response, $crudHook)
 	{
 		$response->setContent([
