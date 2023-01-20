bbd8b3997564cae2ad41f19cc0f24a08a0c8dc54
kelvinwong <KelvinYH_Wong@cncbinternational.com>
2023-01-20 15:02:52 +0800

    Support Manual Flow

--- index.php
+++ index.php
@@ -3,4 +3,16 @@
 namespace YapiSample;
 
 require_once __DIR__ . '/vendor/autoload.php';
-$yapi = new \Kelvinwongg\Yapi\Yapi('oas/api.yaml');
+
+// Auto Flow
+// $yapi = new \Kelvinwongg\Yapi\Yapi('oas/api.yaml');
+
+// Manual Flow
+$yapi = new \Kelvinwongg\Yapi\Yapi();
+$yapi->initResponse();
+$yapi->loadYaml('oas/api.yaml');
+$yapi->loadRequest();
+$yapi->checkRequest($yapi->file, $yapi->request);
+$yapi->loadHook();
+$yapi->execCrud($yapi->request, $yapi->response, $yapi->file, $yapi->hook);
+$yapi->handleResponse($yapi->response);
